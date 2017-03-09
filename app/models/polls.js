7497a01f5341962ac60dcc94adf0314c9b85c9af
gpanagiop <giannis.panagiotopoulos@gmail.com>
2017-03-10 00:29:11 +0200

    added new poll form

--- app/models/polls.js
+++ app/models/polls.js
@@ -3,17 +3,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var voteSchema = new mongoose.Schema({ ip: 'String' });
-var choiceSchema = new mongoose.Schema({ 
-  text: String,
-  votes: [voteSchema]
-});
-
 var Poll = new Schema({
   question: { type: String, required: true },
   choices: {
   	text: String,
-  	votes: Number
+  	votes: {
+  	ip: String
+  	}
   }
 });
 
